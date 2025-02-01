7a1df32373bb6a5216ff22bc83711a7fe4f60969
Даніїл Заболотний <danilzabol@gmail.com>
2025-02-01 15:11:11 +0100

    Switched to supabase. Added efficiency statistics

--- statistics/views.py
+++ statistics/views.py
@@ -1,29 +1,22 @@
-import time
-from typing import List, Optional
-from fastapi import APIRouter, Depends, Query, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Query
 from sqlalchemy.orm import Session
 from admin.dependencies import get_current_admin
 from models.admins import Admin
 from database import get_db
 
-from datetime import datetime
-
 from models.parks import Park
-from renovation.schemas import RenovationStatus, RenovationOut
-from renovation.crud import (
-    create_renovation_db as create_renovation,
-    get_all_renovations_from_db as get_all_renovations,
-    update_renovation_in_db as update_renovation,
-    get_renovation_from_db as get_renovation,
-    delete_renovation_from_db,
-)
 from sqlalchemy import text
+
+from statistics.services.energy_consumption import energy_consumption
+from statistics.services.energy_savings import energy_savings
+from statistics.services.repair_stats import get_avg_repair_time
+from statistics.utils.time_formatters import seconds_to_days_hours
 
 router = APIRouter(prefix="/statistics", tags=["statistics"])
 
 
-@router.post("")
-async def get_statistics(
+@router.post("/park_statistics")
+async def get_park_statistics(
     park_id: int,
     db: Session = Depends(get_db),
     current_admin: Admin = Depends(get_current_admin),
@@ -61,98 +54,44 @@ async def get_statistics(
         dict(zip(columns, row)) for row in planned_renovations.fetchall()
     ]
 
+    avg_seconds = get_avg_repair_time(db, park_id)
+    formatted_time = seconds_to_days_hours(avg_seconds)
+
     return {
-        "top_activated_lanterns": formatted_activated_lanterns,
-        "lanterns_needing_renovation": formatted_needing_renovation,
-        "planned_renovations": formatted_planned_renovations,
+        "Top activated lanterns": formatted_activated_lanterns,
+        "Lanterns needing renovation": formatted_needing_renovation,
+        "Planned renovations": formatted_planned_renovations,
+        "Average repair time": formatted_time,
     }
 
-    # try:
-    #     date_obj = datetime.strptime(date, DATE_FORMAT)
-    #     time_obj = datetime.strptime(time, TIME_FORMAT)
-    #
-    #     datetime_combined = datetime.combine(date_obj, time_obj.time())
-    #
-    #     create_renovation(db, lantern_id, datetime_combined, status)
-    #     return {"message": "Renovation added successfully"}
-    # except ValueError:
-    #     raise HTTPException(
-    #         status_code=400,
-    #         detail=f"Invalid date or time format. Expected formats are {DATE_FORMAT} and {TIME_FORMAT} respectively.",
-    #     )
 
+@router.post("/efficiency_statistics")
+async def get_efficiency_statistics(
+    park_id: int = Query(
+        None,
+        description="If the park is not entered, the statistics will be generalized",
+    ),
+    energy_cost: float = Query(
+        4.32,
+        description="Enter the actual energy cost in the грн/кВт∙год",
+    ),
+    db: Session = Depends(get_db),
+    current_admin: Admin = Depends(get_current_admin),
+):
+    park_filtration = ""
 
-# @router.get("/list", response_model=List[RenovationOut])
-# async def get_renovation_list(
-#     db: Session = Depends(get_db),
-#     current_admin: Admin = Depends(get_current_admin),
-# ):
-#     renovations = get_all_renovations(db)
-#
-#     formatted_renovations = []
-#     for renovation in renovations:
-#         renovation_out = RenovationOut(**vars(renovation))
-#         renovation_out.date = renovation.date.strftime(DATETIME_FORMAT)
-#         formatted_renovations.append(renovation_out)
-#
-#     return formatted_renovations
-#
-#
-# @router.get("/info/{renovation_id}", response_model=RenovationOut)
-# def get_single_renovation(
-#     renovation_id: int,
-#     db: Session = Depends(get_db),
-#     current_admin: Admin = Depends(get_current_admin),
-# ):
-#     renovation = get_renovation(db, renovation_id)
-#
-#     formatted_renovation = RenovationOut(**vars(renovation))
-#     formatted_renovation.date = renovation.date.strftime(DATETIME_FORMAT)
-#     return formatted_renovation
-#
-#
-# @router.put("/update/{renovation_id}", response_model=RenovationOut)
-# def update_renovation_details(
-#     renovation_id: int,
-#     lantern_id: Optional[int] = Query(
-#         None, description="Foreign key of the table 'lanterns'"
-#     ),
-#     date: str = Query(
-#         None,
-#         description=f"Date in format {DATE_FORMAT}",
-#     ),
-#     time: str = Query(
-#         None,
-#         description=f"Time in format {TIME_FORMAT}",
-#     ),
-#     status: Optional[RenovationStatus] = Query(None, description="Renovation status"),
-#     db: Session = Depends(get_db),
-#     current_admin: Admin = Depends(get_current_admin),
-# ):
-#     renovation = update_renovation(
-#         db,
-#         renovation_id,
-#         lantern_id,
-#         date,
-#         time,
-#         DATE_FORMAT,
-#         TIME_FORMAT,
-#         status,
-#     )
-#
-#     formatted_renovation = RenovationOut(**vars(renovation))
-#     formatted_renovation.date = renovation.date.strftime(DATETIME_FORMAT)
-#     return formatted_renovation
-#
-#
-# @router.delete("/delete/{renovation_id}", response_model=RenovationOut)
-# async def delete_renovation(
-#     renovation_id: int,
-#     db: Session = Depends(get_db),
-#     current_admin: Admin = Depends(get_current_admin),
-# ):
-#     renovation = delete_renovation_from_db(db, renovation_id)
-#
-#     formatted_renovation = RenovationOut(**vars(renovation))
-#     formatted_renovation.date = renovation.date.strftime(DATETIME_FORMAT)
-#     return formatted_renovation
+    if park_id:
+        park = db.query(Park).filter(Park.id == park_id).first()
+        if not park:
+            raise HTTPException(status_code=404, detail="Park not found")
+        park_filtration = "WHERE s1.park_id = :park_id"
+
+    total_energy_consumption = energy_consumption(db, park_filtration, park_id)
+    avg_savings_percent, avg_savings = energy_savings(db, park_filtration, park_id)
+    savings_in_money = f"{round(avg_savings * energy_cost, 4)} грн/кВт∙год"
+
+    return {
+        "Total energy consumption": total_energy_consumption,
+        "Average energy savings": avg_savings_percent,
+        "Energy savings in monetary terms": savings_in_money,
+    }
